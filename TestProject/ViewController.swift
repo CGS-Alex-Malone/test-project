efdfdccc91c1999f380614fbc8600539790c120b
Alex Malone <1618@cgs.act.edu.au>
2015-07-21 12:59:10 +1000

    Added some code

--- TestProject/ViewController.swift
+++ TestProject/ViewController.swift
@@ -11,8 +11,11 @@ import UIKit
 class ViewController: UIViewController {
 
     override func viewDidLoad() {
+        
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        
+        // Do some cool stuff here
+        println("Hello World");
     }
 
     override func didReceiveMemoryWarning() {
