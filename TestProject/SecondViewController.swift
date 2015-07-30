618468c9bbd9be2729ae431a048074aec4d1284e
Alex Malone <1618@cgs.act.edu.au>
2015-07-30 15:09:29 +1000

    Added second view controller

---
+++ TestProject/SecondViewController.swift
@@ -0,0 +1,26 @@
+//
+//  SecondViewController.swift
+//  TestProject
+//
+//  Created by Alex on 30/07/2015.
+//  Copyright (c) 2015 Akjmalone. All rights reserved.
+//
+
+import Foundation
+import UIKit
+
+class SecondViewController: UIViewController {
+    
+    override func viewDidLoad() {
+        
+        super.viewDidLoad()
+        
+    }
+    
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+    
+}
